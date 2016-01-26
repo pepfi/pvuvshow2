eb7187d97ce2b68386b477af1947ad478a458115
wangmingguang <1028237180@qq.com>
2016-01-26 18:21:44 +0800

    Update Movie_model.php

--- application/models/Movie_model.php
+++ application/models/Movie_model.php
@@ -16,7 +16,7 @@ class Movie_model extends CI_Model{
     }    
     
     public function get_movies() {
-        $query = $this->db->query("select updatetime, movie_name, movie_play_total from `movie-total`");
+        $query = $this->db->query("select updatetime, movie_name, movie_play_total from `movie-total` order by updatetime desc");
         
         return $query->result_array();
     }   
